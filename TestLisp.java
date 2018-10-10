473374693d7748f5d219472c4e84aa9023d26355
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-10-10 17:29:05 -0400

    Add files via upload

---
+++ TestLisp.java
@@ -0,0 +1,13 @@
+
+public class TestLisp {
+
+	public static void main(String[] args) {
+
+		Lisp p = new Lisp();
+		double add1 = 10.0;
+		double add2 = 5.0;
+		p.getAddStack().push(add1);
+		p.getAddStack().push(add2);
+	}
+
+}
