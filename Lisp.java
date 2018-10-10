a6c4b40c2ac878f7517792ab488fda2eaa8ca597
trzsroo <33383490+trzsroo@users.noreply.github.com>
2018-10-10 16:59:26 -0400

    Add files via upload
    changes

--- Lisp.java
+++ Lisp.java
@@ -1,22 +1,45 @@
 
 public class Lisp {
 	
-	private LinkedStack <Integer> stack;
+	private LinkedStack <Double> stackAdd;
+	private LinkedStack <Double> stackSub;
+	private LinkedStack <Double> stackMult;
+	private LinkedStack <Double> stackDiv;
 	
 	public Lisp() {
-		stack = new LinkedStack <Integer> ();
+		stackAdd = new LinkedStack <Double> ();
+		stackSub = new LinkedStack <Double> ();
+		stackMult = new LinkedStack <Double> ();
+		stackDiv = new LinkedStack <Double> ();
 	}
 	
-	public LinkedStack <Integer> copy() {
-		LinkedStack <Integer> copyBackwards = new LinkedStack <Integer>();
-		LinkedStack <Integer> copyForwards = new LinkedStack <Integer>();
+	public LinkedStack <Double> getAddStack(){
+		return stackAdd;
+	}
+	
+	public LinkedStack <Double> getSubStack(){
+		return stackSub;
+	}
+	
+	public LinkedStack <Double> getMultStack(){
+		return stackMult;
+	}
+	
+	public LinkedStack <Double> getDivStack(){
+		return stackDiv;
+	}
+	
+	
+	public LinkedStack <Double> copy(LinkedStack <Double> stack) {
+		LinkedStack <Double> copyBackwards = new LinkedStack <Double>();
+		LinkedStack <Double> copyForwards = new LinkedStack <Double>();
 		while(!stack.isEmpty()) {
-			int item = stack.pop();
+			double item = stack.pop();
 			copyBackwards.push(item);
 		}
 		
 		while(!stack.isEmpty()) {
-			int item = copyBackwards.pop();
+			double item = copyBackwards.pop();
 			copyForwards.push(item);
 		}
 		
@@ -26,13 +49,32 @@ public class Lisp {
 		
 	}
 	
-	public int addition() {
-		if(stack.isEmpty()) {
+	public LinkedStack <Double> copyBackwards(LinkedStack <Double> stack) {
+		LinkedStack <Double> copyBackwards = new LinkedStack <Double>();
+		LinkedStack <Double> copyForwards = new LinkedStack <Double>();
+		while(!stack.isEmpty()) {
+			double item = stack.pop();
+			copyBackwards.push(item);
+		}
+		
+		while(!stack.isEmpty()) {
+			double item = copyBackwards.pop();
+			copyForwards.push(item);
+		}
+		
+		stack = copyForwards;
+		
+		return copyBackwards;
+		
+	}
+	
+	public double addition() {
+		if(stackAdd.isEmpty()) {
 			return 0;
 		}
 		else {
-			int sum = 0;
-			LinkedStack<Integer> stack2 = copy();
+			double sum = 0;
+			LinkedStack<Double> stack2 = copy(stackAdd);
 			while(!stack2.isEmpty()) {
 				sum += stack2.pop();
 			}
@@ -40,15 +82,29 @@ public class Lisp {
 		}
 	}
 
-	public int subtraction() {
+	public double subtraction() {
+		double sub = 0;
+		LinkedStack<Double> stack2 = copyBackwards(stackSub);
+		if(stackSub.getSize() == 1) {
+			return 0 - stack2.pop();
+		}
+		else if (stackSub.getSize() > 1) {
+			sub = stack2.pop();
+			while(!stack2.isEmpty()) {
+				sub -= stack2.pop();
+			}
+			return sub;
+		}
 		return 0;
 	}
 
-	public int multiplication() {
+	public double multiplication() {
 		return 0;
 	}
 
-	public int division() {
+	public double division() {
+		double div = 0;
+		LinkedStack<Double> stack2 = copyBackwards(stackSub);
 		return 0;
 	}
 
